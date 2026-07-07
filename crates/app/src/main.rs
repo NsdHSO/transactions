cde937bc675601ebb808633010ad0fc70617e279
NsdHSO <nechiforelsamuel@gmail.com>
2026-07-07 15:00:26 +0300

    feat: 🎸 added a gpui components created

--- crates/app/src/main.rs
+++ crates/app/src/main.rs
@@ -1,35 +1,37 @@
 use components::panes::{HelloWorld, HelloWorld1};
-use gpui::{App, AppContext, Application, Bounds, TitlebarOptions, WindowBounds, WindowOptions};
+use gpui::{AsyncApp, Bounds, WindowBounds, WindowOptions, *};
+use gpui_component::Root;
 use split::Split;
 
 fn main() {
-    Application::new().run(|cx: &mut App| {
-        cx.open_window(
-            WindowOptions {
-                focus: true,
-                titlebar: Some(TitlebarOptions {
-                    title: Some("My App".into()),
-                    appears_transparent: true,
-                    traffic_light_position: None,
-                }),
-                window_bounds: Some(WindowBounds::Maximized(Bounds::default())),
-                ..Default::default()
-            },
-            |_, cx| {
-                let left = cx.new(|_| HelloWorld {
-                    text: "World".into(),
-                });
-                let right = cx.new(|_| HelloWorld1 {
-                    text: "World1".into(),
-                });
-                cx.new(|_| Split {
-                    left,
-                    right,
-                    left_width: 500.0,
-                })
-            },
-        )
-        .unwrap();
-        cx.activate(true);
+    let app = gpui_platform::application();
+
+    app.run(move |cx: &mut App| {
+        gpui_component::init(cx);
+
+        cx.spawn(async move |cx: &mut AsyncApp| {
+            cx.open_window(
+                WindowOptions {
+                    focus: true,
+                    window_bounds: Some(WindowBounds::Maximized(Bounds::default())),
+                    ..Default::default()
+                },
+                |window, cx| {
+                    let left = cx.new(|_| HelloWorld {
+                        text: "World".into(),
+                    });
+                    let right = cx.new(|cx| HelloWorld1::new(window, cx));
+                    let split = cx.new(|_| Split {
+                        left,
+                        right,
+                        left_width: 500.0,
+                    });
+                    cx.new(|cx| Root::new(split, window, cx))
+                },
+            )?;
+
+            Ok::<_, anyhow::Error>(())
+        })
+        .detach();
     });
 }
--- crates/components/src/panes/hello_world1.rs
+++ crates/components/src/panes/hello_world1.rs
@@ -1,21 +1,43 @@
-use gpui::{Context, IntoElement, ParentElement, Render, SharedString, Styled, Window, div, rgb};
+use gpui::{
+    AppContext, Context, Entity, IntoElement, ParentElement, Render, SharedString, Styled,
+    Subscription, Window, div, rgb,
+};
+use gpui_component::input::{Input, InputEvent, InputState};
 
 pub struct HelloWorld1 {
-    pub text: SharedString,
+    input_state: Entity<InputState>,
+    value: SharedString,
+    _subscriptions: Vec<Subscription>,
+}
+
+impl HelloWorld1 {
+    pub fn new(window: &mut Window, cx: &mut Context<Self>) -> Self {
+        let input_state = cx.new(|cx| InputState::new(window, cx).placeholder("Type here"));
+
+        let _subscriptions = vec![cx.subscribe_in(&input_state, window, {
+            let input_state = input_state.clone();
+            move |this, _, ev: &InputEvent, _window, cx| match ev {
+                InputEvent::Change => {
+                    this.value = input_state.read(cx).value().clone();
+                    cx.notify();
+                }
+                _ => {}
+            }
+        })];
+
+        Self {
+            input_state,
+            value: SharedString::default(),
+            _subscriptions,
+        }
+    }
 }
 
 impl Render for HelloWorld1 {
     fn render(&mut self, _window: &mut Window, _cx: &mut Context<Self>) -> impl IntoElement {
         div()
             .flex()
-            .flex_col()
-            .gap_3()
-            .bg(rgb(0x3a3a3a))
-            .size_full()
-            .justify_center()
-            .items_center()
-            .text_xl()
-            .text_color(rgb(0xffffff))
-            .child(format!("Hello, {}!", &self.text))
+            .child(div().flex().gap_2().child(Input::new(&self.input_state)))
+            .child(div().text_color(rgb(0x4a6fa5)).child(self.value.clone()))
     }
 }
