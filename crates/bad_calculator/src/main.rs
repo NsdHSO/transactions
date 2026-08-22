29355e2015cb0bf14ec2e965c02cc582eaa2c603
NsdHSO <nechiforelsamuel@gmail.com>
2026-08-22 12:54:48 +0300

    added another aproach with enums, I liked it :D

--- crates/bad_calculator/src/main.rs
+++ crates/bad_calculator/src/main.rs
@@ -3,32 +3,20 @@ use std::io::{Write, stdin, stdout};
 use bad_calculator::evaluat_expression;
 
 fn main() {
-    let mut buffer = String::new();
-    let stdin = stdin();
+    // let mut buffer = String::new();
+    // let stdin = stdin();
 
-    loop {
-        print!("> ");
-        stdout().flush().unwrap();
+    let add_op = Operation::new("+".to_string(), Add);
+    let multiply = Operation::new("*".to_string(), Multiply);
+    println!("3 {} 5 = {}", add_op.symbol, add_op.evaluate(3.0, 5.0));
+    println!("3 {} 5 = {}", multiply.symbol, multiply.evaluate(2.0, 5.0));
+    let op_2 = Operator::Addition {
+        lhs: Value(23.0),
+        rhs: Value(2.0),
+    };
 
-        buffer.clear();
-        if stdin.read_line(&mut buffer).is_err() {
-            break;
-        }
-
-        let input = buffer.trim();
-        if input.is_empty() {
-            continue;
-        }
-
-        if input == "exit" {
-            break;
-        }
-
-        match evaluat_expression(input) {
-            Ok(result) => println!("{result}"),
-            Err(e) => println!("Error: {e}"),
-        }
-    }
+    println!("Operation 2 > : 23.0 + 2.0 = {}", op_2.apply().evaluate())
+    // let operation: Vec<Operation<_>> = vec![add_op, multiply];
 }
 // Traits  look remarkably like Java Interface/abstract classes,
 // They define method signtures that type must implement.
@@ -57,3 +45,98 @@ fn main() {
 // Misusing traits
 // Traits exist to provide contracts that structs can fulfil, which adds expressiveness and
 // flexibility to the language.
+//
+// We Can't have a collection of different Operation< T > types.
+// Enums
+//
+// - carry and encapsulate their own data
+// - allow many subtypes under umbrella of one supertype
+// - make it simple to select behavior based on their type
+// - Enums Have only ONE impl block
+// - Each Variant is an independent type.
+//
+
+trait OperationType {
+    fn calulate(&self, left: f64, right: f64) -> f64;
+    fn precedence(&self) -> u8;
+}
+
+struct Add;
+impl OperationType for Add {
+    #[allow(dead_code)]
+    fn calulate(&self, left: f64, right: f64) -> f64 {
+        left + right
+    }
+
+    #[allow(dead_code)]
+    fn precedence(&self) -> u8 {
+        1
+    }
+}
+
+struct Multiply;
+impl OperationType for Multiply {
+    fn calulate(&self, left: f64, right: f64) -> f64 {
+        left * right
+    }
+
+    fn precedence(&self) -> u8 {
+        2
+    }
+}
+struct Operation<T: OperationType> {
+    symbol: String,
+    op_type: T,
+}
+
+impl<T: OperationType> Operation<T> {
+    fn new(symbol: String, op_type: T) -> Self {
+        Operation { symbol, op_type }
+    }
+
+    fn evaluate(&self, left: f64, right: f64) -> f64 {
+        self.op_type.calulate(left, right)
+    }
+
+    #[allow(dead_code)]
+    fn get_precedence(&self) -> u8 {
+        self.op_type.precedence()
+    }
+}
+
+struct Value(f64);
+
+impl Value {
+    fn evaluate(&self) -> f64 {
+        self.0
+    }
+}
+
+enum Operator {
+    Addition { lhs: Value, rhs: Value },
+    Substraction { lhs: Value, rhs: Value },
+}
+
+impl Operator {
+    fn apply(&self) -> Value {
+        let inner = match self {
+            Operator::Addition { lhs, rhs } => lhs.evaluate() + rhs.evaluate(),
+            Operator::Substraction { lhs, rhs } => lhs.evaluate() - rhs.evaluate(),
+        };
+
+        Value(inner)
+    }
+
+    fn symbol(&self) -> char {
+        match self {
+            Operator::Addition { .. } => '+',
+            Operator::Substraction { .. } => '-',
+        }
+    }
+
+    fn precedence(&self) -> u8 {
+        match self {
+            Operator::Addition { .. } | Operator::Substraction { .. } => 0,
+        }
+    }
+}
